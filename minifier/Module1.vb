29893740123d7ccd0c31fd3e9fc98ccc822dd7de
Eoussama <oussama_gtacj@live.fr>
2018-05-27 15:30:20 +0000

    Path validation and empty args check added

--- minifier/Module1.vb
+++ minifier/Module1.vb
@@ -1,24 +1,30 @@
 ﻿Module Module1
 
     Sub Main(args As String())
+        Dim inputFile As String
         If args.Count >= 1 Then
-            Dim inputFile As String = args(0)
+            inputFile = args(0)
+        Else
+            Console.Write("Please enter a valid target file: ")
+            inputFile = Console.ReadLine()
+        End If
 
-            If IO.File.Exists(inputFile) = False Then
-                Console.WriteLine($"No file under the name of '{inputFile}' found!")
-            Else
-                Dim outputFile As String = IO.Path.GetDirectoryName(inputFile) + "/" + IO.Path.GetFileNameWithoutExtension(inputFile) + ".min" + IO.Path.GetExtension(inputFile)
-                Dim fReader = New IO.StreamReader(inputFile)
-                Dim minified As String = ""
-                Dim regex = New Text.RegularExpressions.Regex("\s")
+        'Console.WriteLine("Path: " + IO.Path.GetFullPath(inputFile))
+        'Console.ReadLine()
+        If IO.File.Exists(inputFile) = False Then
+            Console.WriteLine($"No file under the name of '{inputFile}' found!")
+        Else
+            Dim outputFile As String = inputFile.Insert(inputFile.LastIndexOf("."), ".min")
+            Dim fReader = New IO.StreamReader(inputFile)
+            Dim minified As String = ""
+            Dim regex = New Text.RegularExpressions.Regex("\s")
 
-                minified = regex.Replace(fReader.ReadToEnd, "")
-                fReader.Close()
+            minified = regex.Replace(fReader.ReadToEnd, "")
+            fReader.Close()
 
-                Dim fWriter = New IO.StreamWriter(outputFile, False)
-                fWriter.Write(minified)
-                fWriter.Close()
-            End If
+            Dim fWriter = New IO.StreamWriter(outputFile, False)
+            fWriter.Write(minified)
+            fWriter.Close()
         End If
     End Sub
 
