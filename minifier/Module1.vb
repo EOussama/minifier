49c0e27f23ff3d58f0f3633bf50623f7f0ccf5c8
Eoussama <oussama_gtacj@live.fr>
2018-05-27 16:03:32 +0000

    Better more accurate regex expresion used

--- minifier/Module1.vb
+++ minifier/Module1.vb
@@ -9,15 +9,13 @@
             inputFile = Console.ReadLine()
         End If
 
-        'Console.WriteLine("Path: " + IO.Path.GetFullPath(inputFile))
-        'Console.ReadLine()
         If IO.File.Exists(inputFile) = False Then
             Console.WriteLine($"No file under the name of '{inputFile}' found!")
         Else
             Dim outputFile As String = inputFile.Insert(inputFile.LastIndexOf("."), ".min")
             Dim fReader = New IO.StreamReader(inputFile)
             Dim minified As String = ""
-            Dim regex = New Text.RegularExpressions.Regex("\s")
+            Dim regex = New Text.RegularExpressions.Regex("[\n\t\r]")
 
             minified = regex.Replace(fReader.ReadToEnd, "")
             fReader.Close()
